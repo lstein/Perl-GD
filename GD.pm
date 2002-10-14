536ba347ea941c11f9913c25957657f2c060454d
lstein <lstein>
2002-10-14 13:04:42 +0000

    changed Math::Trig requirement to 1.0

--- GD.pm
+++ GD.pm
@@ -12,7 +12,7 @@ use Symbol 'gensym','qualify_to_ref';
 use Carp 'croak','carp';
 use strict;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $AUTOLOAD);
-$VERSION = "2.01";
+$VERSION = "2.02";
 
 @ISA = qw(Exporter DynaLoader);
 # Items to export into callers namespace by default. Note: do not export
