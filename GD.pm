a1a526760456f7df64c36139b7584e1d4c4542b0
lstein <lstein>
2002-11-05 00:50:05 +0000

    version 2.041

--- GD.pm
+++ GD.pm
@@ -12,7 +12,7 @@ use Symbol 'gensym','qualify_to_ref';
 use Carp 'croak','carp';
 use strict;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $AUTOLOAD);
-$VERSION = "2.04";
+$VERSION = "2.041";
 
 @ISA = qw(Exporter DynaLoader);
 # Items to export into callers namespace by default. Note: do not export
