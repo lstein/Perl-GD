fd06c2b60eb6776805d0f7db9bb51da26fb3c94f
Lincoln Stein <lincoln.stein@gmail.com>
2014-02-18 23:27:09 -0500

    Fix regression tests to run on Ubuntu 12.04 and later 64bit edition

--- GD.pm
+++ GD.pm
@@ -16,7 +16,7 @@ use GD::Polygon;
 
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $AUTOLOAD);
 
-$VERSION = '2.51';
+$VERSION = '2.52';
 
 @ISA = qw(Exporter DynaLoader);
 # Items to export into callers namespace by default. Note: do not export
