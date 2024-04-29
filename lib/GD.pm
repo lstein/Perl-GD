f3c37498ae8cafa35625a5a129ecc36eaa973c45
Reini Urban <reini.urban@gmail.com>
2024-04-29 16:22:36 +0200

    Release 2.79

--- lib/GD.pm
+++ lib/GD.pm
@@ -16,7 +16,7 @@ use GD::Polygon;
 
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $AUTOLOAD);
 
-$VERSION = '2.78';
+$VERSION = '2.79';
 our $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
