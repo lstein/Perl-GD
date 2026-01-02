bcfe6182869f6d441b72cb62ad100d2cb939a62e
Reini Urban <reini.urban@gmail.com>
2026-01-02 14:57:22 +0100

    Release 2.84
    with the latest fixes

--- lib/GD.pm
+++ lib/GD.pm
@@ -16,7 +16,7 @@ use GD::Polygon;
 
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $AUTOLOAD);
 
-$VERSION = '2.83';
+$VERSION = '2.84';
 our $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
